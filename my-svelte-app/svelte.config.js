6993066c70b9d0db02a36fb4052b2a74fbb8d6c7
devonrsa0 <devonrsa@gmail.com>
2025-11-18 14:44:02 +0530

    Updated svelte.config.js

--- my-svelte-app/svelte.config.js
+++ my-svelte-app/svelte.config.js
@@ -1,11 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import sveltePreprocess from 'svelte-preprocess';
 
-export default {
+const config = {
     kit: {
         adapter: adapter({
             fallback: '404.html'
         })
     },
-    preprocess: vitePreprocess()
+    preprocess: sveltePreprocess()
 };
+
+export default config;
